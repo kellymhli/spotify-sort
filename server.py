2202dc25b87b658c77a59ddd1896d1d8d3292524
Kelly <55807683+kellymhli@users.noreply.github.com>
2019-11-04 16:14:17 -0800

    Import modules and connection to server and db

---
+++ server.py
@@ -0,0 +1,32 @@
+from jinja import StrictUndefined
+from flask import Flask, render_template, redirect, request, session
+from flask_debugtoolbar import DebugToolbarExtension
+
+import model
+
+app = Flask(__name__)
+app.secret_key = "ILIKEWIGGLINGTOMUSIC"
+
+# Raise error when an undefined variable is used in Jinja2
+# rather than failing silently
+app.jinja_env.undefined = StrictUndefined
+
+
+@app.route("/")
+def index():
+    """Homepage."""
+
+    pass
+
+
+if __name__ == "__main__":
+
+    # Needs to be true upon invoking DebugToolbarExtension
+    app.debug = True 
+    # Make sure templates, etc. are not cached in debug mode
+    app.jinja_env.auto_reload = add.debug 
+
+    connect_to_db(app)
+    DebugToolbarExtension(app)
+
+    app.run(port=8888, host='0.0.0.0')
