25fb00fbac3f8ae6647d5999911cef680064c10b
Kelly <55807683+kellymhli@users.noreply.github.com>
2019-11-06 14:36:28 -0800

    Write function to print desired information (id, name, and username) of all user playlists

--- authorization.py
+++ authorization.py
@@ -17,7 +17,6 @@ def get_access_token(username):
                                        redirect_uri = SPOTIPY_REDIRECT_URI)
 
     if token:
-        print(token)
         return token 
     else:
         return None
@@ -28,13 +27,36 @@ def authorize(username):
 
     token = get_access_token(username)
     sp = spotipy.Spotify(auth=token)
+    
+    get_playlists(username, sp)
 
-    results = sp.current_user_saved_tracks(limit=50)
-    for item in results['items']:
+
+def get_playlist_tracks(username, playlist_id, sp):
+    """Return all the tracks in a playlist."""
+
+    playlist_tracks = sp.user_playlist_tracks(username, playlist_id)
+    for item in playlist_tracks:
         track = item['track']
         print(f"{track['name']} - {track['artists'][0]['name']}")
+        
+    # results = sp.current_user_saved_tracks(limit=50)
+    # for item in results['items']:
+    #     track = item['track']
+    #     print(f"{track['name']} - {track['artists'][0]['name']}")
 
-    playlists = sp.user_playlists(username)
-    for playlist in playlists['items']:
+
+def get_playlists(username, sp):
+    """Return a list of all user playlists."""
+
+    results = sp.user_playlists(username)
+    playlists = results['items']
+    while results['next']:
+        results = sp.next(results)
+        playlists.extend(results['items'])
+
+    for playlist in playlists:
         if playlist['owner']['id'] == username:
-            print(playlist['name'])
+            print(f"{playlist['id']}|||{playlist['name']}|||{playlist['owner']['id']}")
+
+
+
