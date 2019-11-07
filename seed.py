2fb102ce052918811348131d801610f08d651715
Kelly <55807683+kellymhli@users.noreply.github.com>
2019-11-07 14:45:19 -0800

    Write load_keys and load_matching_keys functions

--- seed.py
+++ seed.py
@@ -31,6 +31,47 @@ def load_playlists():
     db.session.commit()
 
 
+def load_keys():
+    """Load music keys into database."""
+
+    Key.query.delete()
+    
+    for row in open("seed_data/u.keys"):
+        row = row.rstrip()
+        row_list = row.split("|")
+
+        # Parse file to get key id and name
+        key_id = row_list[0]
+        key_name = row_list[1:]  # List as some keys have more than one name
+        print(key_id, key_name)
+
+        # Create new key entry for database.
+        key = Key(key_id = key_id,
+                  key_name = key_name)    
+
+        db.session.add(key)
+
+    db.session.commit()    
+
+
+def load_matching_keys():
+    """Load keys' matching keys into database."""
+
+    MatchingKey.query.delete()
+
+    for row in open("seed_data/u.keymatch"):
+        row = row.rstrip()
+        pair, key, match = row.split("|")
+
+        matching_key = MatchingKey(key_pair = pair,
+                                   key_id = key,
+                                   matching_key = match)
+        
+        db.session.add(matching_key)
+    
+    db.session.commit()
+
+
 if __name__ == "__main__":
     connect_to_db(app)
 
@@ -39,3 +80,5 @@ if __name__ == "__main__":
 
     # Import data into db
     load_playlist()
+    load_keys()
+    load_matching_keys()
