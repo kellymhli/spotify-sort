7aac7d26a2bef6000833c442afa7871da4e487f3
Kelly <55807683+kellymhli@users.noreply.github.com>
2019-11-07 12:19:10 -0800

    Rename authorization.py to api.py, and write load_playlist function in seed.py

--- seed.py
+++ seed.py
@@ -2,4 +2,30 @@
 # and seeds spotify database with that data
 
 import api
-import model
+from model import User, Playlist, TrackPlaylist, Track, Key, MatchingKey
+from server import app
+
+def load_user():
+    """Load user information into database."""
+
+    pass
+
+
+def load_playlists(playlists):
+    """Load playlists into database."""
+    # playlists = [username, [playlist_id, pl_name], [playlist_id, pl_name]]
+    
+    uaer_id = playlist[0]  # username
+
+    for playlist_id, pl_name in playlists[1:]:
+
+        # Create new playlist row
+        playlist = Playlist(playlist_id = playlist_id,
+                            pl_name = pl_name,
+                            user_id = user_id)
+        
+        # Add playlist to database
+        db.session.add(playlist)
+    
+    # Commit added playlists to database.
+    db.session.commit()
