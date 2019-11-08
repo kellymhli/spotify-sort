3aa3ae3dd72aab0dc4004bbbba0d4f0e694127e9
Kelly <55807683+kellymhli@users.noreply.github.com>
2019-11-07 16:15:53 -0800

    Move code from authorization.py to api.py

--- api.py
+++ api.py
@@ -1,6 +1,33 @@
 import spotipy, sys, os
 import spotipy.util as util
-from authorization import token, sp
+
+SPOTIPY_CLIENT_ID = os.environ.get('SPOTIPY_CLIENT_ID')
+SPOTIPY_CLIENT_SECRET = os.environ.get('SPOTIPY_CLIENT_SECRET')
+SPOTIPY_REDIRECT_URI='http://localhost:8888/callback'
+
+def get_access_token(username):
+    """Return access token from Spotify for defined scopes."""
+
+    scope = 'user-library-read'
+
+    # Get access token from Spotify authorization server.
+    token = util.prompt_for_user_token(username, scope, 
+                                    client_id = SPOTIPY_CLIENT_ID, 
+                                    client_secret = SPOTIPY_CLIENT_SECRET, 
+                                    redirect_uri = SPOTIPY_REDIRECT_URI)
+
+    if token:
+        return token 
+    else:
+        return None
+
+
+def authorize(username):
+    """Get Spotify oAuth authroization."""
+
+    token = get_access_token(username)
+    sp = spotify.Spotify(auth=token)
+
 
 def get_playlist_tracks(username, sp, playlist_list=['5vt2cOxZrcn9yVzTTIURJe', '4xP6FbKJ28lbo9JSqJ9MbZ']):
     """Print all the tracks in a playlist."""
