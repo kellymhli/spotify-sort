0f3c208035e6d20ad9975afe22fd5c1149fb66d3
Kelly <55807683+kellymhli@users.noreply.github.com>
2019-11-20 18:30:24 -0800

    Get AJAX request for playlist tracks working

--- static/app.js
+++ static/app.js
@@ -1,18 +1,16 @@
-$('.tracks-btn').on('click', () => {
-    // evt.preventDefault();
-    // const playlist_id = $(this).attr(value);
-    alert('Hello');
+$(document).ready(function(){
+
+    $('.tracks-btn').on('click', (evt) => {
+        evt.preventDefault();
+        console.log("in here");
+        const playlist_id = evt.target.value;
+        console.log(playlist_id);
+    
+        $.get('/playlist-tracks', {'pl':playlist_id}, (tracks) => {
+            console.log(tracks);
+            for (track of tracks) {
+                $(`.${playlist_id} ul`).append(`<li><a href="/tracks/${track.track_id}">${track.track_name}</a></li>`);
+            }
+        });
+    })
 })
-
-// $('.tracks-btn').on('click', (evt) => {
-//     evt.preventDefault();
-//     const playlist_id = $(this).attr(value);
-//     console.log(playlist_id);
-
-//     $.get('/playlist-tracks', {'pl':playlist_id}, (tracks) => {
-//         console.log(tracks);
-//         for (track of tracks) {
-//             $(`.${playlist_id} ul`).append(`<li><a href="/tracks/${track.track_id}">${track.track_name}</a></li>`);
-//         }
-//     });
-// })
