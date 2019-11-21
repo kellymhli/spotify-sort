1e7e1eea579b5b02911e8d622e8bac796064c0ee
Kelly <55807683+kellymhli@users.noreply.github.com>
2019-11-21 15:41:56 -0800

    Display tracks in modal

--- static/app.js
+++ static/app.js
@@ -4,17 +4,19 @@ $(document).ready(function(){  // Load page before adding event handlers
     $('.tracks-btn').on('click', (evt) => {
         evt.preventDefault();
         const playlist_id = evt.target.value;
-        const modal = $(`#modal-${playlist_id}`)
-        const modalOverlay = $(`#modal-overlay-${playlist_id}`)
-        const closeBtn = $(`close-btn-${playlist_id}`)
-
 
         $.get('/playlist-tracks', {'pl':playlist_id}, (tracks) => {
             for (track of tracks) {
                 $(`.${playlist_id} ul`).append(`<li><a href="/tracks/${track.track_id}">${track.track_name}</a></li>`);
             }
         });
+        
+        const modal = $(`#modal-${playlist_id}`)
+        const modalOverlay = $(`#modal-overlay-${playlist_id}`)
+        const closeBtn = $(`close-btn-${playlist_id}`)
+
+        modal.classList.toggle('closed');
+        modalOverlay.classList.toggle('closed');
     })
 
-    
 })
