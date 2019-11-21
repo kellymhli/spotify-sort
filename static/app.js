7ee619e777c21d7881908a62ea7f30bc0df783b5
Kelly <55807683+kellymhli@users.noreply.github.com>
2019-11-20 17:08:20 -0800

    Import jsonify

--- static/app.js
+++ static/app.js
@@ -1,12 +1,18 @@
-$('.tracks-btn').on('dblclick', (evt) => {
-    evt.preventDefault();
-    const playlist_id = $(this).attr(value);
-    console.log(playlist_id);
+$('.tracks-btn').on('click', () => {
+    // evt.preventDefault();
+    // const playlist_id = $(this).attr(value);
+    alert('Hello');
+})
 
-    $.get('/playlist-tracks', {'pl':playlist_id}, (tracks) => {
-        console.log(tracks);
-        for (track of tracks) {
-            $(`.${playlist_id} ul`).append(`<li><a href="/tracks/${track.track_id}">${track.track_name}</a></li>`);
-        }
-    });
-});
+// $('.tracks-btn').on('click', (evt) => {
+//     evt.preventDefault();
+//     const playlist_id = $(this).attr(value);
+//     console.log(playlist_id);
+
+//     $.get('/playlist-tracks', {'pl':playlist_id}, (tracks) => {
+//         console.log(tracks);
+//         for (track of tracks) {
+//             $(`.${playlist_id} ul`).append(`<li><a href="/tracks/${track.track_id}">${track.track_name}</a></li>`);
+//         }
+//     });
+// })
