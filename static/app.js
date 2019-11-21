84346166a2e2dc35b95fc4ac6ee699e806ee7e5a
Kelly <55807683+kellymhli@users.noreply.github.com>
2019-11-20 18:56:43 -0800

    Add comments and change code format

--- static/app.js
+++ static/app.js
@@ -1,13 +1,12 @@
-$(document).ready(function(){
+$(document).ready(function(){  // Load page before adding event handlers
 
+    // Create event listener on tracks bottons
     $('.tracks-btn').on('click', (evt) => {
         evt.preventDefault();
-        console.log("in here");
         const playlist_id = evt.target.value;
-        console.log(playlist_id);
     
         $.get('/playlist-tracks', {'pl':playlist_id}, (tracks) => {
-            console.log(tracks);
+            // console.log(tracks);
             for (track of tracks) {
                 $(`.${playlist_id} ul`).append(`<li><a href="/tracks/${track.track_id}">${track.track_name}</a></li>`);
             }
