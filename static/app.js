2537aa499234258c59d76e6b6faebe519590da37
Kelly <55807683+kellymhli@users.noreply.github.com>
2019-11-20 16:32:22 -0800

    Work on ajax request

--- static/app.js
+++ static/app.js
@@ -1,9 +1,12 @@
-$('.tracks-btn').on('click', (evt) => {
+$('.tracks-btn').on('dblclick', (evt) => {
     evt.preventDefault();
     const playlist_id = $(this).attr(value);
     console.log(playlist_id);
 
-    $.get('/playlist-tracks', {'pl':playlist_id}, (res) => {
-        $(`.${playlist_id}`).html(res.tracks);;
+    $.get('/playlist-tracks', {'pl':playlist_id}, (tracks) => {
+        console.log(tracks);
+        for (track of tracks) {
+            $(`.${playlist_id} ul`).append(`<li><a href="/tracks/${track.track_id}">${track.track_name}</a></li>`);
+        }
     });
 });
