cad77d0048c32c9cf70dfb35e6364321ade04664
Kelly <55807683+kellymhli@users.noreply.github.com>
2019-11-21 17:08:05 -0800

    Add comments to app.js

--- static/app.js
+++ static/app.js
@@ -1,22 +1,26 @@
-$(document).ready(function() {
+$(document).ready(function() {  // Load all html elems before adding evt handlers
     const tracksBtn = $('.tracks-btn')
 
+    // Get tracks from a playlist and display in a modal
     tracksBtn.on('click', (evt) => {
         evt.preventDefault();
         const playlist_id = evt.target.value;
 
+        // Get playlist tracks and add to modal
         $.get('/playlist-tracks', {'pl':playlist_id}, (tracks) => {
             for (track of tracks) {
                 $(`.${playlist_id} ul`).append(`<li><a href="/tracks/${track.track_id}">${track.track_name}</a></li>`);
             }
         });
 
-        const modal = $(`#modal-${playlist_id}`)
-        modal.toggle('show-modal');
-
+        // Close modal upon click
         const closeBtn = $(`#close-btn-${playlist_id}`)
-        closeBtn.on('click', (evt) => {
+        closeBtn.on('click', () => {
             modal.toggle('show-modal');
         });
+
+        // Display the modal
+        const modal = $(`#modal-${playlist_id}`)
+        modal.toggle('show-modal');
     });
 })
